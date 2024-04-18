916e0c9547ca900e57ff3ddce9b7b012c1c0bc8a
pinkstar0121 <582127421@qq.com>
2024-04-18 21:22:02 +0800

    Site updated: 2024-04-18 21:21:58

--- js/utils.js
+++ js/utils.js
@@ -1,35 +1,38 @@
 const btf = {
-  debounce: (func, wait = 0, immediate = false) => {
+  debounce: function (func, wait, immediate) {
     let timeout
-    return (...args) => {
-      const later = () => {
+    return function () {
+      const context = this
+      const args = arguments
+      const later = function () {
         timeout = null
-        if (!immediate) func(...args)
+        if (!immediate) func.apply(context, args)
       }
       const callNow = immediate && !timeout
       clearTimeout(timeout)
       timeout = setTimeout(later, wait)
-      if (callNow) func(...args)
+      if (callNow) func.apply(context, args)
     }
   },
 
-  throttle: function (func, wait, options = {}) {
+  throttle: function (func, wait, options) {
     let timeout, context, args
     let previous = 0
+    if (!options) options = {}
 
-    const later = () => {
+    const later = function () {
       previous = options.leading === false ? 0 : new Date().getTime()
       timeout = null
       func.apply(context, args)
       if (!timeout) context = args = null
     }
 
-    const throttled = (...params) => {
+    const throttled = function () {
       const now = new Date().getTime()
       if (!previous && options.leading === false) previous = now
       const remaining = wait - (now - previous)
       context = this
-      args = params
+      args = arguments
       if (remaining <= 0 || remaining > wait) {
         if (timeout) {
           clearTimeout(timeout)
@@ -59,12 +62,11 @@ const btf = {
     const { position, bgLight, bgDark } = GLOBAL_CONFIG.Snackbar
     const bg = document.documentElement.getAttribute('data-theme') === 'light' ? bgLight : bgDark
     Snackbar.show({
-      text,
+      text: text,
       backgroundColor: bg,
-      showAction,
-      duration,
-      pos: position,
-      customClass: 'snackbar-css'
+      showAction: showAction,
+      duration: duration,
+      pos: position
     })
   },
 
@@ -76,21 +78,31 @@ const btf = {
     const hour = minute * 60
     const day = hour * 24
     const month = day * 30
-    const { dateSuffix } = GLOBAL_CONFIG
 
-    if (!more) return parseInt(dateDiff / day)
+    let result
+    if (more) {
+      const monthCount = dateDiff / month
+      const dayCount = dateDiff / day
+      const hourCount = dateDiff / hour
+      const minuteCount = dateDiff / minute
 
-    const monthCount = dateDiff / month
-    const dayCount = dateDiff / day
-    const hourCount = dateDiff / hour
-    const minuteCount = dateDiff / minute
-
-    if (monthCount > 12) return datePost.toISOString().slice(0, 10)
-    if (monthCount >= 1) return `${parseInt(monthCount)} ${dateSuffix.month}`
-    if (dayCount >= 1) return `${parseInt(dayCount)} ${dateSuffix.day}`
-    if (hourCount >= 1) return `${parseInt(hourCount)} ${dateSuffix.hour}`
-    if (minuteCount >= 1) return `${parseInt(minuteCount)} ${dateSuffix.min}`
-    return dateSuffix.just
+      if (monthCount > 12) {
+        result = datePost.toLocaleDateString().replace(/\//g, '-')
+      } else if (monthCount >= 1) {
+        result = parseInt(monthCount) + ' ' + GLOBAL_CONFIG.date_suffix.month
+      } else if (dayCount >= 1) {
+        result = parseInt(dayCount) + ' ' + GLOBAL_CONFIG.date_suffix.day
+      } else if (hourCount >= 1) {
+        result = parseInt(hourCount) + ' ' + GLOBAL_CONFIG.date_suffix.hour
+      } else if (minuteCount >= 1) {
+        result = parseInt(minuteCount) + ' ' + GLOBAL_CONFIG.date_suffix.min
+      } else {
+        result = GLOBAL_CONFIG.date_suffix.just
+      }
+    } else {
+      result = parseInt(dateDiff / day)
+    }
+    return result
   },
 
   loadComment: (dom, callback) => {
@@ -109,8 +121,7 @@ const btf = {
 
   scrollToDest: (pos, time = 500) => {
     const currentPos = window.pageYOffset
-    const isNavFixed = document.getElementById('page-header').classList.contains('fixed')
-    if (currentPos > pos || isNavFixed) pos = pos - 70
+    if (currentPos > pos) pos = pos - 70
 
     if ('scrollBehavior' in document.documentElement.style) {
       window.scrollTo({
@@ -152,13 +163,42 @@ const btf = {
     ele.style.animation = text
   },
 
-  wrap: (selector, eleType, options) => {
-    const createEle = document.createElement(eleType)
-    for (const [key, value] of Object.entries(options)) {
-      createEle.setAttribute(key, value)
+  getParents: (elem, selector) => {
+    for (; elem && elem !== document; elem = elem.parentNode) {
+      if (elem.matches(selector)) return elem
     }
-    selector.parentNode.insertBefore(createEle, selector)
-    createEle.appendChild(selector)
+    return null
+  },
+
+  siblings: (ele, selector) => {
+    return [...ele.parentNode.children].filter((child) => {
+      if (selector) {
+        return child !== ele && child.matches(selector)
+      }
+      return child !== ele
+    })
+  },
+
+  /**
+   * @param {*} selector
+   * @param {*} eleType the type of create element
+   * @param {*} options object key: value
+   */
+  wrap: (selector, eleType, options) => {
+    const creatEle = document.createElement(eleType)
+    for (const [key, value] of Object.entries(options)) {
+      creatEle.setAttribute(key, value)
+    }
+    selector.parentNode.insertBefore(creatEle, selector)
+    creatEle.appendChild(selector)
+  },
+
+  unwrap: el => {
+    const elParentNode = el.parentNode
+    if (elParentNode !== document.body) {
+      elParentNode.parentNode.insertBefore(el, elParentNode)
+      elParentNode.parentNode.removeChild(elParentNode)
+    }
   },
 
   isHidden: ele => ele.offsetHeight === 0 && ele.offsetWidth === 0,
@@ -179,11 +219,17 @@ const btf = {
     const service = GLOBAL_CONFIG.lightbox
 
     if (service === 'mediumZoom') {
-      mediumZoom(ele, { background: 'var(--zoom-bg)' })
+      const zoom = mediumZoom(ele)
+      zoom.on('open', e => {
+        const photoBg = document.documentElement.getAttribute('data-theme') === 'dark' ? '#121212' : '#fff'
+        zoom.update({
+          background: photoBg
+        })
+      })
     }
 
     if (service === 'fancybox') {
-      Array.from(ele).forEach(i => {
+      ele.forEach(i => {
         if (i.parentNode.tagName !== 'A') {
           const dataSrc = i.dataset.lazySrc || i.src
           const dataCaption = i.title || i.alt || ''
@@ -195,30 +241,7 @@ const btf = {
         Fancybox.bind('[data-fancybox]', {
           Hash: false,
           Thumbs: {
-            showOnStart: false
-          },
-          Images: {
-            Panzoom: {
-              maxScale: 4
-            }
-          },
-          Carousel: {
-            transition: 'slide'
-          },
-          Toolbar: {
-            display: {
-              left: ['infobar'],
-              middle: [
-                'zoomIn',
-                'zoomOut',
-                'toggle1to1',
-                'rotateCCW',
-                'rotateCW',
-                'flipX',
-                'flipY'
-              ],
-              right: ['slideshow', 'thumbs', 'close']
-            }
+            autoStart: false
           }
         })
         window.fancyboxRun = true
@@ -226,20 +249,19 @@ const btf = {
     }
   },
 
-  setLoading: {
-    add: ele => {
-      const html = `
-        <div class="loading-container">
-          <div class="loading-item">
-            <div></div><div></div><div></div><div></div><div></div>
-          </div>
-        </div>
-      `
-      ele.insertAdjacentHTML('afterend', html)
-    },
-    remove: ele => {
-      ele.nextElementSibling.remove()
-    }
+  initJustifiedGallery: function (selector) {
+    selector.forEach(function (i) {
+      if (!btf.isHidden(i)) {
+        fjGallery(i, {
+          itemSelector: '.fj-gallery-item',
+          rowHeight: 220,
+          gutter: 4,
+          onJustify: function () {
+            this.$container.style.opacity = '1'
+          }
+        })
+      }
+    })
   },
 
   updateAnchor: (anchor) => {
@@ -248,49 +270,8 @@ const btf = {
       const title = GLOBAL_CONFIG_SITE.title
       window.history.replaceState({
         url: location.href,
-        title
+        title: title
       }, title, anchor)
     }
-  },
-
-  getScrollPercent: (currentTop, ele) => {
-    const docHeight = ele.clientHeight
-    const winHeight = document.documentElement.clientHeight
-    const headerHeight = ele.offsetTop
-    const contentMath = (docHeight > winHeight) ? (docHeight - winHeight) : (document.documentElement.scrollHeight - winHeight)
-    const scrollPercent = (currentTop - headerHeight) / (contentMath)
-    const scrollPercentRounded = Math.round(scrollPercent * 100)
-    const percentage = (scrollPercentRounded > 100) ? 100 : (scrollPercentRounded <= 0) ? 0 : scrollPercentRounded
-    return percentage
-  },
-
-  addGlobalFn: (key, fn, name = false, parent = window) => {
-    const globalFn = parent.globalFn || {}
-    const keyObj = globalFn[key] || {}
-
-    if (name && keyObj[name]) return
-
-    name = name || Object.keys(keyObj).length
-    keyObj[name] = fn
-    globalFn[key] = keyObj
-    parent.globalFn = globalFn
-  },
-
-  addEventListenerPjax: (ele, event, fn, option = false) => {
-    ele.addEventListener(event, fn, option)
-    btf.addGlobalFn('pjax', () => {
-      ele.removeEventListener(event, fn, option)
-    })
-  },
-
-  removeGlobalFnEvent: (key, parent = window) => {
-    const { globalFn = {} } = parent
-    const keyObj = globalFn[key] || {}
-    const keyArr = Object.keys(keyObj)
-    if (!keyArr.length) return
-    keyArr.forEach(i => {
-      keyObj[i]()
-    })
-    delete parent.globalFn[key]
   }
 }
